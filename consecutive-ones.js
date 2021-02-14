0cdda14c0b6ef6ab6d3966e0269beaa39c8d2cd9
Lekshmi Chandra <35682999+lek890@users.noreply.github.com>
2021-02-14 12:58:46 +0100

    Update consecutive-ones.js

--- consecutive-ones.js
+++ consecutive-ones.js
@@ -9,7 +9,7 @@ Note:
 
 The input array will only contain 0 and 1.
 The length of input array is a positive integer and will not exceed 10,000
-
+```
 var findMaxConsecutiveOnes = function(nums) {
     
     if(nums.length === 1) {
@@ -37,3 +37,4 @@ var findMaxConsecutiveOnes = function(nums) {
     
     return maxCount;
 };
+```
